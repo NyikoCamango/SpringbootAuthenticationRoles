cf5c729bda4f1f4e6124186070e9ec317ff6063a
NyikoCamango <217026744@student.uj.ac.za>
2023-07-18 11:09:40 +0200

    added Getters, attributes,setters to the User Model

--- SpringSecurity/src/main/java/com/security/SpringSecurity/models/Role.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/models/Role.java
@@ -1,4 +1,25 @@
 package com.security.SpringSecurity.models;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
 public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
 }
