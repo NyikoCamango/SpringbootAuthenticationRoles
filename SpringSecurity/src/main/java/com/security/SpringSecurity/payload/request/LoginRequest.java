35ce4dc80261c3cb6a542c2836534f1d5ef679d3
NyikoCamango <217026744@student.uj.ac.za>
2023-07-19 10:46:07 +0200

    validation for Login and Register

--- SpringSecurity/src/main/java/com/security/SpringSecurity/payload/request/LoginRequest.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/payload/request/LoginRequest.java
@@ -1,4 +1,27 @@
 package com.security.SpringSecurity.payload.request;
 
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequest {
+    @NotBlank (message = "username is required")
+    private String username;
+
+    @NotBlank(message = "username is required")
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
