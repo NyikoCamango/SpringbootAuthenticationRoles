24a9621524dfb9ea6e5fa715bf78f8acd4b264e1
NyikoCamango <217026744@student.uj.ac.za>
2023-07-24 09:39:19 +0200

    Config

--- SpringSecurity/src/main/java/com/security/SpringSecurity/config/MailConfig.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/config/MailConfig.java
@@ -1,4 +1,31 @@
 package com.security.SpringSecurity.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
 public class MailConfig {
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private Integer port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        return mailSender;
+    }
 }
