577d65b02c669f58aae737553bb654bd85c04b1d
NyikoCamango <217026744@student.uj.ac.za>
2023-07-24 10:10:51 +0200

    changed userService

--- SpringSecurity/src/main/java/com/security/SpringSecurity/services/UserService.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/services/UserService.java
@@ -2,7 +2,7 @@ package com.security.SpringSecurity.services;
 
 import com.security.SpringSecurity.models.User;
 
-public class UserService {
+public interface UserService {
     User findByEmail(String email);
     User findByResetToken(String resetToken);
     void save(User user);
--- SpringSecurity/src/main/java/com/security/SpringSecurity/services/UserServiceImpl.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/services/UserServiceImpl.java
@@ -1,4 +1,28 @@
 package com.security.SpringSecurity.services;
 
-public class UserServiceImpl {
+import com.security.SpringSecurity.models.User;
+import com.security.SpringSecurity.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public User findByResetToken(String resetToken) {
+        return userRepository.findByResetToken(resetToken);
+    }
+
+    @Override
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
