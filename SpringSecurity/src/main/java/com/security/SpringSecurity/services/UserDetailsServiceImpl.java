89c978e08f4b97327083848b2821e5aa3fdc7a81
NyikoCamango <217026744@student.uj.ac.za>
2023-07-24 09:57:19 +0200

    UserDetails which implements the service

--- SpringSecurity/src/main/java/com/security/SpringSecurity/services/UserDetailsServiceImpl.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/services/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.security.SpringSecurity.services;
 
-public class UserDetailsServiceImpl {
+import com.security.SpringSecurity.models.User;
+import com.security.SpringSecurity.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
 }
