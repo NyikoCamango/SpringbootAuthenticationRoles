c3a4508d8cbfc81556ad053d931cccf432f72988
NyikoCamango <217026744@student.uj.ac.za>
2023-07-25 12:25:36 +0200

    dto for RestPasswordDto

--- SpringSecurity/src/main/java/com/security/SpringSecurity/dto/ResetPasswordDto.java
+++ SpringSecurity/src/main/java/com/security/SpringSecurity/dto/ResetPasswordDto.java
@@ -1,4 +1,42 @@
 package com.security.SpringSecurity.dto;
 
 public class ResetPasswordDto {
+    private String email;
+    private String newPassword;
+    private String confirmPassword;
+    private String resetToken;
+
+    // getters and setters
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public String getResetToken() {
+        return resetToken;
+    }
+
+    public void setResetToken(String resetToken) {
+        this.resetToken = resetToken;
+    }
 }
